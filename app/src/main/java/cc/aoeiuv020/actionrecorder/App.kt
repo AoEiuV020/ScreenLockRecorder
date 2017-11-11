76f026593bdec303bfddedd05be8e1d36746dfca
AoEiuV020 <aoeiuv020@gmail.com>
2017-11-11 20:50:01 +0800

    Create project

---
+++ app/src/main/java/cc/aoeiuv020/actionrecorder/App.kt
@@ -0,0 +1,9 @@
+package cc.aoeiuv020.actionrecorder
+
+import android.app.Application
+
+/**
+ *
+ * Created by AoEiuV020 on 2017.11.11-20:47:15.
+ */
+class App : Application()
---
+++ app/src/main/java/cc/aoeiuv020/actionrecorder/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package cc.aoeiuv020.actionrecorder.main
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import cc.aoeiuv020.actionrecorder.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
