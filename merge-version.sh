f93c3e761f5845ced51e48dcf91de889776f7e24
AoEiuV020 <aoeiuv020@gmail.com>
2018-08-26 21:13:51 +0800

    add merge-version.sh

---
+++ merge-version.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# 自动建个release分支合并到master，
+# 然后删除这个分支，
+set -e
+old=$PWD
+cd $(dirname $0)
+project=$(pwd)
+buildGradleFile="$project/build.gradle"
+
+versionName=$(sed -n 's/\s*version_name\s*=\s*"\(\S*\)"/\1/p' $buildGradleFile)
+
+branch=release-$versionName
+git checkout -b $branch
+git checkout master
+git merge --no-ff $branch
+git branch -d $branch
+git tag -a $versionName
+git checkout dev
+git merge master
+
